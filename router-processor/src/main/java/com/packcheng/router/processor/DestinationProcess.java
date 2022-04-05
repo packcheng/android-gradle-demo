bf214a324a95880672f8034be12a632bedf80e02
packcheng <packcheng_jo@outlook.com>
2022-04-05 16:15:14 +0800

    feat: 增加注解处理器生成json文档和插件生成md文档

--- router-processor/src/main/java/com/packcheng/router/processor/DestinationProcess.java
+++ router-processor/src/main/java/com/packcheng/router/processor/DestinationProcess.java
@@ -1,24 +1,55 @@
 package com.packcheng.router.processor;
 
+import static com.packcheng.router.util.Consts.KEY_DOC_DESCRIPTION;
+import static com.packcheng.router.util.Consts.KEY_DOC_REAL_PATH;
+import static com.packcheng.router.util.Consts.KEY_DOC_URL;
+import static com.packcheng.router.util.Consts.KEY_ROOT_PROJECT_DIR;
+import static com.packcheng.router.util.Consts.METHOD_GET;
+import static com.packcheng.router.util.Consts.NAME_OF_FILE_MAPPING;
+import static com.packcheng.router.util.Consts.NAME_OF_FOLDER_MAPPING_DOC;
+import static com.packcheng.router.util.Consts.NAME_OF_JSON;
+import static com.packcheng.router.util.Consts.NAME_OF_MAPPING;
+import static com.packcheng.router.util.Consts.NO_ROOT_PROJECT_DIR_TIPS;
+import static com.packcheng.router.util.Consts.PACKAGE_OF_GENERATE_DOCS;
+import static com.packcheng.router.util.Consts.PACKAGE_OF_GENERATE_FILE;
+import static com.packcheng.router.util.Consts.WARNING_TIPS;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.google.auto.service.AutoService;
 import com.packcheng.router.annotations.Destination;
+import com.packcheng.router.util.Logger;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.tools.StandardLocation;
 
 /**
  * {@link com.packcheng.router.annotations.Destination}的注解处理器
@@ -31,6 +62,41 @@ import javax.lang.model.element.TypeElement;
 public class DestinationProcess extends AbstractProcessor {
     private static final String TAG = "DestinationProcess";
 
+    Filer mFiler;
+    Logger logger;
+    private Writer docWriter;       // Writer used for write doc
+    private String rootDir;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        mFiler = processingEnv.getFiler();
+        logger = new Logger(processingEnv.getMessager());
+
+        // Attempt to get user configuration [moduleName]
+        Map<String, String> options = processingEnv.getOptions();
+        if (MapUtils.isNotEmpty(options)) {
+            rootDir = options.get(KEY_ROOT_PROJECT_DIR);
+        }
+
+        if (StringUtils.isNotEmpty(rootDir)) {
+            logger.info("The user has configuration the root_project_dir, it was [" + rootDir + "]");
+        } else {
+            logger.error(NO_ROOT_PROJECT_DIR_TIPS);
+            throw new RuntimeException("ZbcRouter::Compiler >>> No root_project_dir, for more information, look at gradle log.");
+        }
+
+        try {
+            docWriter = mFiler.createResource(
+                    StandardLocation.SOURCE_OUTPUT,
+                    PACKAGE_OF_GENERATE_DOCS,
+                    "arouter-map-of-" + System.currentTimeMillis() + ".json"
+            ).openWriter();
+        } catch (IOException e) {
+            logger.error("Create doc writer failed, because " + e.getMessage());
+        }
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         // 避免多次调用process
@@ -38,29 +104,42 @@ public class DestinationProcess extends AbstractProcessor {
             return false;
         }
 
-        System.out.println(TAG + ">>> process start ...");
-
-        // 获取所有标记了@Destination注解的 类的信息
-        Set<? extends Element> allDestinationElements = roundEnv.getElementsAnnotatedWith(Destination.class);
-        System.out.println(TAG + ">>> all Destination elements count = " + allDestinationElements.size());
-
-        // 未收集到@Destination注解信息的时候，跳过后续的处理
-        if (allDestinationElements.size() < 1) {
-            return false;
+        if (CollectionUtils.isNotEmpty(annotations)) {
+            Set<? extends Element> routeElements = roundEnv.getElementsAnnotatedWith(Destination.class);
+            try {
+                logger.info(TAG + ">>> process start ...");
+                this.parseRoutes(routeElements);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+            System.out.println(TAG + ">>> process finished ...");
+            return true;
         }
 
-        ClassName map = ClassName.get("java.util", "Map");
-        ClassName hashMap = ClassName.get("java.util", "HashMap");
-        ClassName str = ClassName.get("java.lang", "String");
+        return false;
+    }
+
+    private void parseRoutes(Set<? extends Element> routeElements) throws IOException {
+        // 未收集到@Destination注解信息的时候，跳过后续的处理
+        if (CollectionUtils.isEmpty(routeElements)) {
+            return;
+        }
+
+        ClassName map = ClassName.get(Map.class);
+        ClassName hashMap = ClassName.get(HashMap.class);
+        ClassName str = ClassName.get(String.class);
         ParameterizedTypeName mapOfStrStr = ParameterizedTypeName.get(map, str, str);
 
-        MethodSpec.Builder getBuilder = MethodSpec.methodBuilder("get")
+        MethodSpec.Builder getBuilder = MethodSpec.methodBuilder(METHOD_GET)
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .returns(mapOfStrStr)
                 .addStatement("$T mapping = new $T<>()", mapOfStrStr, hashMap);
 
+        List<Map<String, String>> docSource = new ArrayList<>();
+        Map<String, String> routerInfoMap;
+
         // 遍历所有 @Destination 注解信息，挨个获取详细信息
-        for (Element element : allDestinationElements) {
+        for (Element element : routeElements) {
             final TypeElement typeElement = (TypeElement) element;
 
             // 尝试在当前类中获取 @Destination注解信息
@@ -76,28 +155,66 @@ public class DestinationProcess extends AbstractProcessor {
 
             getBuilder.addStatement("mapping.put($S,$S)", url, realPath);
 
-            System.out.println(TAG + ">>> url= " + url);
-            System.out.println(TAG + ">>> description= " + description);
-            System.out.println(TAG + ">>> realPath= " + realPath);
+            routerInfoMap = new HashMap<>();
+            routerInfoMap.put(KEY_DOC_URL, url);
+            routerInfoMap.put(KEY_DOC_DESCRIPTION, description);
+            routerInfoMap.put(KEY_DOC_REAL_PATH, realPath);
+            docSource.add(routerInfoMap);
+
+            logger.info(TAG + ">>> url= " + url);
+            logger.info(TAG + ">>> description= " + description);
+            logger.info(TAG + ">>> realPath= " + realPath);
         }
 
         getBuilder.addStatement("return mapping");
-        TypeSpec routerMappingClass = TypeSpec.classBuilder("RouterMapping_" + System.currentTimeMillis())
-                .addModifiers(Modifier.PUBLIC)
-                .addMethod(getBuilder.build())
-                .build();
 
-        JavaFile javaFile = JavaFile.builder("com.packcheng.routerapp", routerMappingClass)
-                .build();
-        try {
-            javaFile.writeTo(processingEnv.getFiler());
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Output route doc
+        final String jsonDoc = JSON.toJSONString(docSource, SerializerFeature.PrettyFormat);
+        docWriter.append(jsonDoc);
+        docWriter.flush();
+        docWriter.close();
+        genDocOnRootDir(jsonDoc);
+
+        // Write provider into disk
+        JavaFile.builder(PACKAGE_OF_GENERATE_FILE,
+                TypeSpec.classBuilder(NAME_OF_MAPPING + System.currentTimeMillis())
+                        .addJavadoc(WARNING_TIPS)
+                        .addModifiers(Modifier.PUBLIC)
+                        .addMethod(getBuilder.build())
+                        .build())
+                .build().writeTo(mFiler);
+    }
+
+    /**
+     * 在根目录创建生成json文档
+     *
+     * @param jsonDoc 需要生成文档的json格式字符串
+     */
+    private void genDocOnRootDir(String jsonDoc) throws IOException {
+        if (StringUtils.isEmpty(jsonDoc)
+                || StringUtils.isEmpty(rootDir)) {
+            logger.error("Empty rootDir or json info");
+            return;
         }
 
-        System.out.println(TAG + ">>> process finished ...");
+        File rootFile = new File(rootDir);
+        if (!rootFile.exists()) {
+            logger.error("RootDir not exits");
+            return;
+        }
 
-        return false;
+        File jsonFileDir = new File(rootFile, NAME_OF_FOLDER_MAPPING_DOC);
+        if (!jsonFileDir.exists()) {
+            jsonFileDir.mkdirs();
+        }
+
+        final String jsonFileName = NAME_OF_FILE_MAPPING + System.currentTimeMillis() + NAME_OF_JSON;
+        File jsonFile = new File(jsonFileDir, jsonFileName);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(jsonFile));
+        bufferedWriter.write(jsonDoc, 0, jsonDoc.length());
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 
     /**
---
+++ routerapp/src/main/java/com/packcheng/routerapp/KotlinActivity.kt
@@ -0,0 +1,14 @@
+package com.packcheng.routerapp
+
+import com.packcheng.router.annotations.Destination
+
+/**
+ *
+ *
+ * @author packcheng <a href="mailto:packcheng_jo@outlook.com">Contact me.</a>
+ * @version 1.0
+ * @since 2022/4/5 12:22
+ */
+@Destination(url = "router://kotlin",description = "测试kotlin页面")
+ class KotlinActivity {
+}
